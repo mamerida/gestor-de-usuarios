8cc4301a56a48e5ce110568a3fc1940efd40b2f8
Mario <mamerida2013@gmail.com>
2021-12-27 09:59:27 -0300

    programacion de la funcion submit, impresion de usuarios ingresados en pantalla

--- src/App.js
+++ src/App.js
@@ -5,20 +5,37 @@ import Card from './components/Card'
 import Container from './components/Container'
 import Button from './components/Button'
 function App() {
-  const [formulario,handleChange] = useFormulario({name: " ", lastname:" " ,email:" "})
+  const [usuarios,setUsuarios] = useState([])
+  const [formulario,handleChange, reset] = useFormulario({name: " ", lastname:" " ,email:" "})
+  //boton submit lo que hace es evitar el comportamiento que actualiza la url y ademas al arreglo de usuarios le concatena el valor del formulario
+  const submit = e =>{
+    e.preventDefault()
+    setUsuarios([
+      ...usuarios,
+      formulario
+    ])
+    reset()
+  }
 
 
   return (
     <Container>
       <Card>
         <div style={{padding:20}} >
-          <form>
+          <form onSubmit={submit}>
             <Input  label="Nombre" name="name" value={formulario.name} onChange={handleChange}/>
             <Input  label="Apellido" name="lastname" value={formulario.lastname} onChange={handleChange}/>
             <Input  label="Correo" name="email" value={formulario.email} onChange={handleChange}/>
             <Button>Enviar </Button>
           </form>
         </div>
+      </Card>
+      <Card>
+        <ul>
+          {usuarios.map(x =>
+              <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>
+            )}
+        </ul>
       </Card>
     </Container> 
   );
--- src/hooks/useFormulario.js
+++ src/hooks/useFormulario.js
@@ -1,12 +1,17 @@
 import {useState} from 'react'
 
+
 const useFormulario = (inicial) =>{
     const [formulario, setFormulario] = useState(inicial)
     const handleChange = (e) =>{
       setFormulario({...formulario,[e.target.name] : e.target.value})
     }
-
-    return [formulario,handleChange]
+    
+    const reset = () =>{
+        setFormulario(inicial)
+    }
+    
+    return [formulario,handleChange ,reset]
 }
 
 export default useFormulario
