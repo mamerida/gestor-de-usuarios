35e0c0612d920f33e61bc40b526ab979da006520
Mario <mamerida2013@gmail.com>
2021-12-27 09:38:43 -0300

    se coloca los inputs dentro de un card y un container tambien homologados para darle mejor estilo a la pantalla todo mediante los archivos de css correspondiente

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import {useState} from 'react'
 import useFormulario from './hooks/useFormulario'
 import Input from './components/Input'
+import Card from './components/Card'
+import Container from './components/Container'
 function App() {
   const [formulario,handleChange] = useFormulario({name: " ", lastname:" "})
 
 
   return (
-    <form>
-      <Input  label="Nombre" name="name" value={formulario.name} onChange={handleChange}/>
-      <Input  label="Apellido" name="lastname" value={formulario.lastname} onChange={handleChange}/>
-    </form>
+    <Container>
+      <Card>
+        <div style={{padding:20}} >
+          <form>
+            <Input  label="Nombre" name="name" value={formulario.name} onChange={handleChange}/>
+            <Input  label="Apellido" name="lastname" value={formulario.lastname} onChange={handleChange}/>
+          </form>
+        </div>
+      </Card>
+    </Container> 
   );
 }
 
